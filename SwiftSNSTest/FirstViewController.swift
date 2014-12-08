b30f71e90fa5c43a7fcb9a4c4f5d2ac4fabc9fec
kawamurashin <kawamura@mztm.jp>
2014-12-08 17:42:13 +0900

    Initial Commit

---
+++ SwiftSNSTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwiftSNSTest
+//
+//  Created by 川村森 on 2014/12/08.
+//  Copyright (c) 2014年 kawamura shin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftSNSTest/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SwiftSNSTest
+//
+//  Created by 川村森 on 2014/12/08.
+//  Copyright (c) 2014年 kawamura shin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
